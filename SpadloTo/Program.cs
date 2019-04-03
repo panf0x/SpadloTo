3d4fbea0b7b078230824763806eac8bcdf07e7af
Lukas Hnojsky <hnojsky.l@gmail.com>
2019-04-03 12:28:54 +0200

    Comment of main method

--- SpadloTo/Program.cs
+++ SpadloTo/Program.cs
@@ -9,14 +9,14 @@ namespace SpadloTo
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application Jolanda Crash Detector
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.SetCompatibleTextRenderingDefault( false );
+            Application.Run( new Form1() );
         }
     }
 }
